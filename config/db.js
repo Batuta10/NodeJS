05d25b954c94023c34b0261565ccde91c713a9b3
Batuta10 <batuta2604@gmail.com>
2017-08-17 08:34:30 -0300

    remove console.log

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 var conecMyslq = function(){
-    console.log('Conexao foi estabelecida');
     return mysql.createConnection({
             host: 'localhost',
             user: 'root',
@@ -10,6 +9,5 @@ var conecMyslq = function(){
 }
 
 module.exports = function(){
-    console.log('Conexao f');
     return conecMyslq;
 }
