4427bf76eec99d4811c967a96dfbd2f92bcef798
logonrm <logonrm@fiap.com.br>
2017-08-24 11:27:09 -0300

    Not finish bdoyparse

--- app/routes/addProduto.js
+++ app/routes/addProduto.js
@@ -2,4 +2,8 @@ module.exports = function(app){
     app.get('/addProduto',function(req, res){
 	    res.render('admin/form_add_produto');
     });
+    app.post('/produtos/salvar', (req, res)=> {
+	    var produto = req.body;
+        res.send(produto);
+    });
 };
