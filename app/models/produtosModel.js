8ccd21b9ef9c51cecc3aeff70779332e6dde9afb
Batuta10 <batuta2604@gmail.com>
2017-08-30 09:28:19 -0300

    Add Model clientes  and upgrade produtosModel

--- app/models/produtosModel.js
+++ app/models/produtosModel.js
@@ -5,5 +5,8 @@ module.exports = function(){
     this.getProduto = function(connection, callback){
         connection.query('SELECT * FROM produtos WHERE id_produtos = 1', callback);
     }//GetProdutos
+    this.addProduto = function(produto, connection, callback){
+        connection.query('INSERT INTO produtos SET ?', produto, callback);
+    }
     return this;
 }//Module
