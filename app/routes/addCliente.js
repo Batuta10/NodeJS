23af1590e679b591055af238bea3da9b2b09b892
logonrm <logonrm@fiap.com.br>
2017-09-06 09:34:54 -0300

    Implementation of express-validator

--- app/routes/addCliente.js
+++ app/routes/addCliente.js
@@ -1,10 +1,25 @@
 module.exports = function(app){
     app.get('/addCliente',function(req, res){
-	    res.render('admin/form_add_cliente');
+	    res.render('admin/form_add_cliente',{validacao : {}, cliente : {}});
     });
 
     app.post('/clientes/salvar', (req, res)=> {
         var cliente = req.body;
+        
+        req.assert('nome', 'Nome é obrigatorio não pode passar de 45 ou ser menor que 3 letras').len(3,45);
+        req.assert('sobrenome', 'Sobrenome é obrigatorio não pode passar de 45 ou ser menor que 3 letras').len(3,45);
+        //req.assert('dt_nascimento', 'Data de nascimento é obrigatorio').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('cep', 'Cep é obrigatorio e deve ter 11 letras').len(8,8);
+        req.assert('endereco', 'Endereço é obrigatorionao não pode passar de 45 ou ser menor que 3 letras').len(3,45);
+        req.assert('cpf', 'CPF é obrigatorio e deve ter 11 letras').len(11,11);
+
+        var erros = req.validationErrors();
+
+        if (erros) {
+            res.render('admin/form_add_cliente', {validacao : erros, cliente : cliente});
+            return;
+        }
+
 
         var connection = app.config.db();
         var clientesModel = new app.app.models.clientesDAO(connection);
--- app/routes/addProduto.js
+++ app/routes/addProduto.js
@@ -1,10 +1,21 @@
 module.exports = function(app){
     app.get('/addProduto',function(req, res){
-	    res.render('admin/form_add_produto');
+	    res.render('admin/form_add_produto', {validacao : {}, produto : {}});
     });
     app.post('/produtos/salvar', (req, res)=> {
         var produto = req.body;
 
+        req.assert('nome', 'Nome é obrigatorio').notEmpty();
+        req.assert('descricao', 'Descrição é obrigatorio').notEmpty();
+        
+        var erros = req.validationErrors();
+        
+        console.log(erros);
+
+        if (erros) {
+            res.render('admin/form_add_produto', {validacao : erros, produto : produto});
+            return;
+        }
         var connection = app.config.db();
         var produtosModel = new app.app.models.produtosDAO(connection);
     
